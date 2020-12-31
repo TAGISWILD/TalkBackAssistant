8ad3ad61671d43706039532adaca45a641707b90
Atharva Chauhan <46600294+TAGISWILD@users.noreply.github.com>
2020-12-31 21:53:55 +0530

    Add files via upload

---
+++ Utils/Setup.bat
@@ -0,0 +1,8 @@
+pip install PyAudio-0.2.11-cp38-cp38-win_amd64.whl
+pip install -r requirements.txt
+attrib -h -s PyAudio-0.2.11-cp38-cp38-win_amd64.whl
+attrib -h -s requirements.txt
+cls
+color 0a
+echo All files are installed sucessfully and you are ready to take off
+pause
